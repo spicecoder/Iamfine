f56223ef1677e8277cc17d88ea0ecfe08117c427
Ayas <ayasknayak6238@gmail.com>
2023-09-18 17:19:04 +0530

    ping-pong one is here!!!

---
+++ Notification/client/ping-pong/src/App.js
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+import { onMessageListener } from "./firebase";
+import Notification from "./notification"; // Assuming this is the correct import
+import { displayCurrentToken } from "./firebase";
+
+//const API_BASE_URL = "http://localhost:5020";
+
+const App = () => {
+ 
+  const [show, setShow] = useState(false);
+  const [notification, setNotification] = useState({ title: "", body: "" });
+  // const [item, setItem] = useState('');
+  // const [showItem, setShowItem] = useState(false);
+  // //const token = "";
+  const [userInput, setUserInput] = useState("");
+  const [userInput1, setUserInput1] = useState("");
+
+ 
+  const showNotification = (title, body) => {
+    setNotification({ title, body });
+    setShow(true);
+  };
+  // const handleClick = () => {
+  //   const newItem = displayCurrentToken;
+  //   setItem(newItem);
+  //   setShowItem(true);
+  // };
+
+  const recieve = (e) => {
+    e.preventDefault(); // Prevent the form from submitting and refreshing the page
+    //console.log("User input:", userInput); 
+    fetch("http://localhost:5020/recieve", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    })
+   
+  };
+
+  const recieveToken = (e) => {
+    e.preventDefault(); // Prevent the form from submitting and refreshing the page
+    //console.log("User input:", userInput); 
+    fetch("http://localhost:5020/recieveToken", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput1 }),
+    })
+   
+  };
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const payload = await onMessageListener();
+        showNotification(
+          payload?.notification?.title,
+          payload?.notification?.body
+        );
+      } catch (err) {
+        console.log('Failed: ', err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  
+  return (
+    <div className="Notification-App">
+       <div>
+     
+      <button onClick={displayCurrentToken}>Display Token</button>
+      <div id="result-box"></div>
+    </div>
+      <form onSubmit={recieve} name="token_receiving">
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)} // Update user input state
+          required
+          placeholder="Enter Text"
+        />
+        <button type="submit">SEND MSG TO NABAKUMAR</button>
+      </form>
+      <form onSubmit={recieveToken} name="token_receiving">
+        <input
+          type="text"
+          value={userInput1}
+          onChange={(e) => setUserInput1(e.target.value)} // Update user input state
+          required
+          placeholder="Enter Token"
+        />
+        <button type="submit">SEND FCM TOKEN </button>
+      </form>
+      
+      {show ? (
+        <Notification title={notification.title} body={notification.body} />
+      ) : (
+        <></>
+      )}
+      <Notification />
+
+      
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ Notification/client/ping-pong/src/firebase.js
@@ -0,0 +1,71 @@
+
+
+
+import { initializeApp } from 'firebase/app'
+import {getAuth} from 'firebase/auth'
+import {getMessaging,getToken,onMessage} from 'firebase/messaging'
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAJA0esCg6gB0rUBBm6iBmcSIanrW4GAQ4",
+
+  authDomain: "khlogin-01.firebaseapp.com",
+
+  databaseURL: "https://khlogin-01.firebaseio.com",
+
+  projectId: "khlogin-01",
+
+  storageBucket: "khlogin-01.appspot.com",
+
+  messagingSenderId: "405781304253",
+
+  appId: "1:405781304253:web:d82a78fc10c387a85978d4",
+
+  measurementId: "G-L887L4HMFE"
+
+};
+// Initialize Firebase and Firebase Authentication
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+const messaging = getMessaging();
+let token = null;
+export {app}
+export {auth}
+export const  requestForToken= () => {
+  return getToken(messaging, { vapidKey: `BCV1N_G2oj3_tQuXmM78QQVje_WdkYDyqbUONJMaUMovfDrPKTs8mbSktvgoptdJxHwtSjck_0xs3T-aNCxFsXQ` })
+    .then((currentToken) => {
+      if (currentToken) {
+        token = currentToken;
+       // currentToken = token;
+
+        console.log('current token for client: ', currentToken);
+        // Perform any other neccessary action with the token
+      } else {
+        // Show permission request UI
+        console.log('No registration token available. Request permission to generate one.');
+      }
+     
+    })
+    .catch((err) => {
+      console.log('An error occurred while retrieving token. ', err);
+    });
+};
+
+export const onMessageListener = () =>
+  new Promise((resolve) => {
+    onMessage(messaging, (payload) => {
+      console.log("payload", payload)
+      resolve(payload);
+    });
+  });
+
+
+  export const displayCurrentToken = () => {
+    
+    const resultElement = document.getElementById('result-box');
+  
+    if (token) {
+      resultElement.textContent = 'Current Token: ' + token;
+    } else {
+      resultElement.textContent = 'No current token available. Please request one.';
+    }
+  };
